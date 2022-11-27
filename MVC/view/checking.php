f6686bc2a32f25192eaf05af61cf7ad18881db82
ishrat jahan <106507090+ishrat3@users.noreply.github.com>
2022-11-27 13:52:24 +0600

    Add files via upload

--- MVC/view/checking.php
+++ MVC/view/checking.php
@@ -1,37 +1,19 @@
 <!DOCTYPE html>
 <html>
+<p id="demo"></p>
+<script src="js/checking.js"></script>
 <body>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="/controller/checkingAction.php"novalidate onsubmit="return is valid(this)">
 <fieldset>	
 <legend>Checking</legend>	
-<label for="voter_nid">voter_nid:</label>
 <br>
 <input type="int" id="voter_nid" name="voter_nid">
 <br>
-<label for="voter_wordno">voter_wordno:</label>
-<br>
-<input type="int" id="voter_wordno" name="voter_wordno">
-<br>
-<label for="voter_age">voter_age:</label>
-<br>
-<input type="int" id="voter_age" name="voter_age">
+Age: <input type="int" name="age">
+<?php echo isset($_SESSION['age_msg']) ? $_SESSION['age_msg'] : ""; ?>
 <br>
 <p class="center"><input value="Checking" type="submit" class="center" /></p>
 </form>
 </fieldset>
-<?php
-session_start();
- 
-  if($_SERVER["REQUEST_METHOD"] == "POST"){
-     $_SESSION['login_user']=$voter_nid;
- 
-     header("Location: verified.html");
-    }
-    else 
-    {
-    $error="Your voter_nid is invalid";
-    }	
-
-?>
 </body>
 </html>
