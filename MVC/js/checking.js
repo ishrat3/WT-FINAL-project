1e3a237c6666e0bf7a94f41e5b1afaa31d6335e9
ishrat jahan <106507090+ishrat3@users.noreply.github.com>
2022-11-27 13:02:52 +0600

    Add files via upload

---
+++ MVC/js/checking.js
@@ -0,0 +1,13 @@
+let age;    
+function myFunction(x){
+for (let i = 0; i < 100; i++) {
+age += "The number is " + i + "<br>";
+}
+if (age < 18){
+console.log("voter_nid exist");
+}else
+{console.log("voter_nid not exist");
+}
+
+myFunction(10)
+}
